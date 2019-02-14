e2c78ec1329c44465f48a4089196f4f8ed1e07e9
mandodz2019 <46611116+mandodz2019@users.noreply.github.com>
2019-02-14 15:15:16 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("520345626915897346")
+setInterval(function() {
+channel.send(`mohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xDmohadz2019 xD`);
+}, 30)
+})
+
+client.login("NDYzMDE0ODQyMjUzNDQzMDgy.Dzts_Q.U3ec3qyyh3hUoSaIII1Ac5ZkGyI");
